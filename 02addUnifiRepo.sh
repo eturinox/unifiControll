4102496ac4fa8af29d14ad534697291594d0923a
eturinox <39201765+eturinox@users.noreply.github.com>
2019-08-15 21:36:01 +0200

    Update 02addUnifiRepo.sh

--- 02addUnifiRepo.sh
+++ 02addUnifiRepo.sh
@@ -1,1 +1,2 @@
 echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+echo 'deb http://www.ui.com/downloads/unifi/debian release ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
